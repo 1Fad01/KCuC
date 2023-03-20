68bdd271af546f4bce5b123c2cf2ff3a01e154f5
1Fad01 <108678819+1Fad01@users.noreply.github.com>
2023-03-20 10:20:57 +0300

    lab3-6
    xd

---
+++ labs KCuC/lab3-6/lab3-6/Program.cs
@@ -0,0 +1,8 @@
+﻿string fileName = @"\\Desktop-0lj979m\ksis\lab3-6.txt";
+using (FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+{
+    using (StreamWriter writer = new StreamWriter(stream))
+    {
+        writer.WriteLine("Лабораторную работу выполняли: студенты группы 10701121 Длусский К.Ю. и Качан И.В.");
+    }
+}
